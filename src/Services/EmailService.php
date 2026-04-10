046591316205f0efedcc3720993240077017daea
BLUE WATER <kurguzov654@gmail.com>
2026-04-10 16:34:15 +0300

    fix: fixed mutable PHPMailer object

--- src/Services/EmailService.php
+++ src/Services/EmailService.php
@@ -8,50 +8,50 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 class EmailService
 {
-    private PHPMailer $mail;
-
     public function __construct(private array $config)
     {
-        $this->mail = new PHPMailer(true);
-        $this->setSettings();
     }
-    private function setSettings(): void
+
+    private function createMailer(): PHPMailer
     {
-        $this->mail->SMTPDebug = (int) ($this->config['debug'] ?? 0);
-        $this->mail->isSMTP();
-        $this->mail->CharSet = PHPMailer::CHARSET_UTF8;
-        $this->mail->Host = $this->config['host'];
-        $this->mail->SMTPAuth = (bool) ($this->config['auth'] ?? false);
-        $this->mail->Username = (string) ($this->config['username'] ?? '');
-        $this->mail->Password = (string) ($this->config['password'] ?? '');
-        $this->mail->Port = (int) $this->config['port'];
+        $mail = new PHPMailer(true);
+        $mail->SMTPDebug = (int) ($this->config['debug'] ?? 0);
+        $mail->isSMTP();
+        $mail->CharSet = PHPMailer::CHARSET_UTF8;
+        $mail->Host = $this->config['host'];
+        $mail->SMTPAuth = (bool) ($this->config['auth'] ?? false);
+        $mail->Username = (string) ($this->config['username'] ?? '');
+        $mail->Password = (string) ($this->config['password'] ?? '');
+        $mail->Port = (int) $this->config['port'];
 
         // Mailpit / local SMTP: plain text on 1025. Real providers: tls/ssl via MAIL_ENCRYPTION.
         $enc = strtolower((string) ($this->config['encryption'] ?? ''));
         if ($enc === 'ssl' || $enc === 'smtps') {
-            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         } elseif ($enc === 'tls' || $enc === 'starttls') {
-            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         } else {
-            $this->mail->SMTPSecure = false;
-            $this->mail->SMTPAutoTLS = false;
+            $mail->SMTPSecure = false;
+            $mail->SMTPAutoTLS = false;
         }
+        return $mail;
     }
 
     public function sendMessage(string $toEmail, string $message): void
     {
+        $mail = $this->createMailer();
 
-        $this->mail->setFrom($this->config['fromEmail'], $this->config['fromName']);
-        $this->mail->addAddress($toEmail);
+        $mail->setFrom($this->config['fromEmail'], $this->config['fromName']);
+        $mail->addAddress($toEmail);
 
-        $this->mail->isHTML(true);
-        $this->mail->Subject = 'Цена на товар изменилась! Pricerr';
-        $this->mail->Body = $message;
-        $this->mail->AltBody = strip_tags($message);
+        $mail->isHTML(true);
+        $mail->Subject = 'Цена на товар изменилась! Pricerr';
+        $mail->Body = $message;
+        $mail->AltBody = strip_tags($message);
 
-        $isSended = $this->mail->send();
+        $isSended = $mail->send();
         if (!$isSended) {
-            throw new \Exception('Failed to send a message. ' . $this->mail->ErrorInfo);
+            throw new \Exception('Failed to send a message. ' . $mail->ErrorInfo);
         }
     }
 }
